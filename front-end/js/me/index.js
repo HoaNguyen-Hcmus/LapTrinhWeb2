743b5f3581eeec87a27081e02686f7651a1522bc
xuanhieu073 <32063031+xuanhieu073@users.noreply.github.com>
2018-05-26 20:46:01 +0700

    mau index

--- front-end/js/me/index.js
+++ front-end/js/me/index.js
@@ -1,17 +1,48 @@
-$.ajax({
+/*$.ajax({
 	url: 'http://localhost:3000',
 	type: 'GET',
 	dataType: 'json',
 	data: {},
 })
-.done(function(data) {
-	$("#test").html(data.mess);
-	swal('Đã load trang test');
-})
-.fail(function(error) {
-	console.log(error);
+	.done(function (data) {
+		$("#test").html(data.mess);
+		swal('Đã load trang test');
+	})
+	.fail(function (error) {
+		console.log(error);
+	});*/
+$("#btn-loguot").on('click', function () {
+	localStorage.access_token = undefined;
 });
 
-$("#btn-loguot").on('click', function() {
-	sessionStorage.removeItem('access_token');
+$( document ).ready(function(){
+	HandlebarsIntl.registerWith(Handlebars);
+	loadTop5RaGia();
 });
+
+var loadTop5RaGia = function () {
+	$('.loader').show();
+
+	$.ajax({
+		url: 'http://localhost:3000/login',
+		dataType: 'json',
+		timeout: 10000
+	}).done(function (data) {
+		var source = $('#product-template').html();
+		var template = Handlebars.compile(source);
+		var html = template(data);
+		console.log(html);
+		$('#Top5RaGia-list').append(html);
+
+		$('#Top5RaGia-list div[style]').fadeIn(1000, function () {
+			$(this).removeAttr('style');
+		});
+
+        //CUR_PAGE++;
+        if (data.hasMore === false) {
+            $('#btnMore').hide();
+        }
+
+        $('.loader').hide();
+	});
+};

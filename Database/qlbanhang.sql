449049087bb4e7035efa2596cfce79a786eb4ac7
huynvp <huynvp@gmail.com>
2018-06-13 10:17:41 +0700

    fix code sql
    Thêm columns ở 1 số bảng

--- Database/qlbanhang.sql
+++ Database/qlbanhang.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Máy chủ: 127.0.0.1
--- Thời gian đã tạo: Th5 21, 2018 lúc 03:16 PM
+-- Thời gian đã tạo: Th6 13, 2018 lúc 05:16 AM
 -- Phiên bản máy phục vụ: 10.1.31-MariaDB
 -- Phiên bản PHP: 7.2.4
 
@@ -41,7 +41,7 @@ CREATE TABLE `admin` (
 --
 
 INSERT INTO `admin` (`ID`, `NAME`, `ADDRESS`, `EMAIL`, `PHONE`) VALUES
-(1, 'Nguyễn Quang Huy', '748/16/6 Hồng Bàng Quận 11', 'huydeptrai@ahihi.com', '0123456789');
+(101, 'Nguyễn Quang Huy', '748/16/6 Hồng Bàng Quận 11', 'huydeptrai@ahihi.com', '0123456789');
 
 -- --------------------------------------------------------
 
@@ -52,8 +52,8 @@ INSERT INTO `admin` (`ID`, `NAME`, `ADDRESS`, `EMAIL`, `PHONE`) VALUES
 CREATE TABLE `caplaimatkhau` (
   `ID` int(11) NOT NULL,
   `NguoiDung` int(11) NOT NULL,
-  `ThoiGian` datetime NOT NULL,
-  `TrangThai` tinyint(1) NOT NULL
+  `ThoiGian` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `TrangThai` tinyint(1) NOT NULL DEFAULT '0'
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 -- --------------------------------------------------------
@@ -65,8 +65,26 @@ CREATE TABLE `caplaimatkhau` (
 CREATE TABLE `danhmuc` (
   `ID` int(11) NOT NULL,
   `Ten` varchar(200) NOT NULL,
-  `TrangThai` tinyint(1) NOT NULL
+  `TrangThai` tinyint(1) NOT NULL DEFAULT '1',
+  `NgayThem` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `NgaySuaDoi` datetime DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Đang đổ dữ liệu cho bảng `danhmuc`
+--
+
+INSERT INTO `danhmuc` (`ID`, `Ten`, `TrangThai`, `NgayThem`, `NgaySuaDoi`) VALUES
+(1, 'Điện thoại', 1, '2018-05-25 14:38:18', NULL),
+(2, 'Máy tính', 1, '2018-05-25 14:38:18', '2018-05-26 10:50:51'),
+(3, 'Test', 0, '2018-05-26 10:20:36', '2018-05-26 10:48:29'),
+(4, 'Test', 0, '2018-05-26 10:20:45', '2018-05-26 15:40:24'),
+(5, '', 0, '2018-05-26 10:21:27', '2018-05-26 10:52:09'),
+(6, '', 0, '2018-05-26 10:21:50', NULL),
+(7, 'Phim ảnh', 0, '2018-05-26 10:22:01', NULL),
+(8, '', 0, '2018-05-26 10:23:35', NULL),
+(9, 'Hình ảnh', 1, '2018-05-26 15:40:03', NULL),
+(10, 'Test', 0, '2018-05-26 15:40:19', NULL);
 
 -- --------------------------------------------------------
 
@@ -80,6 +98,13 @@ CREATE TABLE `danhsachyeuthich` (
   `SanPham` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
+--
+-- Đang đổ dữ liệu cho bảng `danhsachyeuthich`
+--
+
+INSERT INTO `danhsachyeuthich` (`ID`, `NguoiDung`, `SanPham`) VALUES
+(1, 1, 1);
+
 -- --------------------------------------------------------
 
 --
@@ -92,10 +117,20 @@ CREATE TABLE `daugia` (
   `NguoiRaGia` int(11) NOT NULL,
   `GiaDuaRa` decimal(10,0) NOT NULL,
   `GiaDauTuDong` decimal(10,0) NOT NULL,
-  `TrangThaiKick` tinyint(1) NOT NULL,
-  `CoThangCuoc` tinyint(1) NOT NULL,
+  `TrangThaiKick` tinyint(1) NOT NULL DEFAULT '0',
+  `CoThangCuoc` tinyint(1) NOT NULL DEFAULT '0',
   `ThoiGianDuaRa` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Đang đổ dữ liệu cho bảng `daugia`
+--
+
+INSERT INTO `daugia` (`ID`, `SanPham`, `NguoiRaGia`, `GiaDuaRa`, `GiaDauTuDong`, `TrangThaiKick`, `CoThangCuoc`, `ThoiGianDuaRa`) VALUES
+(1, 1, 3, '17000000', '0', 0, 0, '2018-06-10 22:10:16'),
+(2, 2, 3, '17000000', '17000000', 0, 0, '2018-06-10 22:10:37'),
+(3, 3, 3, '17500000', '17500000', 0, 0, '2018-06-10 22:11:04'),
+(4, 1, 2, '17200000', '17200000', 0, 0, '2018-06-10 22:11:21');
 
 -- --------------------------------------------------------
 
@@ -117,9 +152,10 @@ CREATE TABLE `login` (
 --
 
 INSERT INTO `login` (`ID`, `USERNAME`, `PASSWORD`, `LAST_LOGIN`, `WRONG_PASS`, `USER_ID`) VALUES
-(1, 'admin', '21232f297a57a5a743894a0e4a801fc3', '2018-05-16 13:54:26', 0, 1),
-(2, 'huynvp', 'e10adc3949ba59abbe56e057f20f883e', '2018-05-19 20:58:33', 0, 0),
-(3, 'huynvp', 'e10adc3949ba59abbe56e057f20f883e', '2018-05-19 20:58:42', 0, 2);
+(1, 'admin', '21232f297a57a5a743894a0e4a801fc3', '2018-05-17 13:54:00', 0, 101),
+(2, 'huynvp', 'e10adc3949ba59abbe56e057f20f883e', '2018-05-19 20:58:33', 0, 1),
+(3, 'huy', 'e10adc3949ba59abbe56e057f20f883e', '2018-05-19 20:58:42', 0, 2),
+(4, 'nvhoa', 'e10adc3949ba59abbe56e057f20f883e', '2018-06-11 22:25:38', 0, 2);
 
 -- --------------------------------------------------------
 
@@ -133,6 +169,16 @@ CREATE TABLE `mota` (
   `MoTa` varchar(2500) NOT NULL,
   `ThoiGian` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Đang đổ dữ liệu cho bảng `mota`
+--
+
+INSERT INTO `mota` (`ID`, `SanPham`, `MoTa`, `ThoiGian`) VALUES
+(1, 1, '<p><a href=\"https://www.thegioididong.com/laptop/acer-aspire-a315-51-39dj-i3-7130u-nxgnpsv030\"><strong>Laptop Acer Aspire A315 51 39DJ</strong></a><strong> là mẫu máy tính tầm trung trang bị chip Intel Core i3 Kabylake (thế hệ 7) hoạt động rất ổn định và có một thiết kế mang đậm chất mạnh mẽ, tinh tế.</strong></p><p><strong>Thiết kế góc cạnh</strong></p><p>Mẫu <a href=\"https://www.thegioididong.com/laptop-acer\">laptop Acer</a> mình muốn giới thiệu là mẫu <a href=\"https://www.thegioididong.com/laptop\">máy tính xách tay</a> nằm trong phân khúc tầm trung, có thiết kế bằng chất liệu vỏ nhựa, 4 góc cạnh vuông vức giúp máy mạnh mẽ và đầy tinh tế. Máy có trọng lượng 2.1 kg và dày 21.6 mm giúp bạn dễ dàng khi di chuyển máy mọi lúc, mọi nơi.</p><p><strong>Cấu hình thế hệ thứ 7</strong></p><p>Mẫu <a href=\"https://www.thegioididong.com/laptop-acer?g=core-i3\">laptop Acer Core i3</a> này thuộc <a href=\"https://www.thegioididong.com/hoi-dap/bo-xu-ly-intel-core-i3-i5-i7-the-he-thu-7-952583\">CPU thế hệ thứ 7</a> của Intel cho khả năng vận hành hệ thống ổn định. Cùng <a href=\"https://www.thegioididong.com/hoi-dap/ram-ddr4-la-gi-882173\">RAM DDR4</a> 4 GB giúp máy hoạt động tốt trong các trường hợp mở nhiều ứng dụng. Ổ cứng được sử dụng ở đây là loại <a href=\"https://www.thegioididong.com/hoi-dap/hdd-la-gi-922791\">HDD</a> 1 TB thì khá thoải mái để bạn lưu trữ dữ liệu cá nhân.</p><p><strong>Màn hình lớn</strong></p><p>Đi kèm với thân máy là một màn hình tích hợp 15.6 inch to lớn, dễ nhìn và cùng với độ phân giải <a href=\"https://www.thegioididong.com/hoi-dap/man-hinh-fhd-la-gi-956294\">Full HD</a>cho bạn trải nghiệm không gian thoải mái để giải trí hay làm việc.</p><p><strong>Bàn phím kích thước to rõ</strong></p><p>Với kích thước bàn phím lớn cho bạn thoải mái nhập liệu văn bản, và cụm số quen thuộc như trên máy tính để bàn sẽ giúp bạn làm việc nhanh hơn.</p><p><strong>Multi Touchpad</strong></p><p><a href=\"https://www.thegioididong.com/hoi-dap/multi-touchpad-la-gi-920569\">Touchpad</a> thông minh chỉ nhận lệnh khi phát hiện đó là đầu ngón tay đang điều khiển và hạn chế được việc lòng bàn tay vô tình chạm vào bị tương tác sai.</p>', '2018-06-10 10:54:29'),
+(2, 2, '<p><a href=\"https://www.thegioididong.com/laptop/acer-aspire-a315-51-39dj-i3-7130u-nxgnpsv030\"><strong>Laptop Acer Aspire A315 51 39DJ</strong></a><strong> là mẫu máy tính tầm trung trang bị chip Intel Core i3 Kabylake (thế hệ 7) hoạt động rất ổn định và có một thiết kế mang đậm chất mạnh mẽ, tinh tế.</strong></p><p><strong>Thiết kế góc cạnh</strong></p><p>Mẫu <a href=\"https://www.thegioididong.com/laptop-acer\">laptop Acer</a> mình muốn giới thiệu là mẫu <a href=\"https://www.thegioididong.com/laptop\">máy tính xách tay</a> nằm trong phân khúc tầm trung, có thiết kế bằng chất liệu vỏ nhựa, 4 góc cạnh vuông vức giúp máy mạnh mẽ và đầy tinh tế. Máy có trọng lượng 2.1 kg và dày 21.6 mm giúp bạn dễ dàng khi di chuyển máy mọi lúc, mọi nơi.</p><p><strong>Cấu hình thế hệ thứ 7</strong></p><p>Mẫu <a href=\"https://www.thegioididong.com/laptop-acer?g=core-i3\">laptop Acer Core i3</a> này thuộc <a href=\"https://www.thegioididong.com/hoi-dap/bo-xu-ly-intel-core-i3-i5-i7-the-he-thu-7-952583\">CPU thế hệ thứ 7</a> của Intel cho khả năng vận hành hệ thống ổn định. Cùng <a href=\"https://www.thegioididong.com/hoi-dap/ram-ddr4-la-gi-882173\">RAM DDR4</a> 4 GB giúp máy hoạt động tốt trong các trường hợp mở nhiều ứng dụng. Ổ cứng được sử dụng ở đây là loại <a href=\"https://www.thegioididong.com/hoi-dap/hdd-la-gi-922791\">HDD</a> 1 TB thì khá thoải mái để bạn lưu trữ dữ liệu cá nhân.</p><p><strong>Màn hình lớn</strong></p><p>Đi kèm với thân máy là một màn hình tích hợp 15.6 inch to lớn, dễ nhìn và cùng với độ phân giải <a href=\"https://www.thegioididong.com/hoi-dap/man-hinh-fhd-la-gi-956294\">Full HD</a>cho bạn trải nghiệm không gian thoải mái để giải trí hay làm việc.</p><p><strong>Bàn phím kích thước to rõ</strong></p><p>Với kích thước bàn phím lớn cho bạn thoải mái nhập liệu văn bản, và cụm số quen thuộc như trên máy tính để bàn sẽ giúp bạn làm việc nhanh hơn.</p><p><strong>Multi Touchpad</strong></p><p><a href=\"https://www.thegioididong.com/hoi-dap/multi-touchpad-la-gi-920569\">Touchpad</a> thông minh chỉ nhận lệnh khi phát hiện đó là đầu ngón tay đang điều khiển và hạn chế được việc lòng bàn tay vô tình chạm vào bị tương tác sai.</p>', '2018-06-10 11:14:57'),
+(3, 3, '<p><a href=\"https://www.thegioididong.com/laptop/acer-aspire-a315-51-39dj-i3-7130u-nxgnpsv030\"><strong>Laptop Acer Aspire A315 51 39DJ</strong></a><strong> là mẫu máy tính tầm trung trang bị chip Intel Core i3 Kabylake (thế hệ 7) hoạt động rất ổn định và có một thiết kế mang đậm chất mạnh mẽ, tinh tế.</strong></p><p><strong>Thiết kế góc cạnh</strong></p><p>Mẫu <a href=\"https://www.thegioididong.com/laptop-acer\">laptop Acer</a> mình muốn giới thiệu là mẫu <a href=\"https://www.thegioididong.com/laptop\">máy tính xách tay</a> nằm trong phân khúc tầm trung, có thiết kế bằng chất liệu vỏ nhựa, 4 góc cạnh vuông vức giúp máy mạnh mẽ và đầy tinh tế. Máy có trọng lượng 2.1 kg và dày 21.6 mm giúp bạn dễ dàng khi di chuyển máy mọi lúc, mọi nơi.</p><p><strong>Cấu hình thế hệ thứ 7</strong></p><p>Mẫu <a href=\"https://www.thegioididong.com/laptop-acer?g=core-i3\">laptop Acer Core i3</a> này thuộc <a href=\"https://www.thegioididong.com/hoi-dap/bo-xu-ly-intel-core-i3-i5-i7-the-he-thu-7-952583\">CPU thế hệ thứ 7</a> của Intel cho khả năng vận hành hệ thống ổn định. Cùng <a href=\"https://www.thegioididong.com/hoi-dap/ram-ddr4-la-gi-882173\">RAM DDR4</a> 4 GB giúp máy hoạt động tốt trong các trường hợp mở nhiều ứng dụng. Ổ cứng được sử dụng ở đây là loại <a href=\"https://www.thegioididong.com/hoi-dap/hdd-la-gi-922791\">HDD</a> 1 TB thì khá thoải mái để bạn lưu trữ dữ liệu cá nhân.</p><p><strong>Màn hình lớn</strong></p><p>Đi kèm với thân máy là một màn hình tích hợp 15.6 inch to lớn, dễ nhìn và cùng với độ phân giải <a href=\"https://www.thegioididong.com/hoi-dap/man-hinh-fhd-la-gi-956294\">Full HD</a>cho bạn trải nghiệm không gian thoải mái để giải trí hay làm việc.</p><p><strong>Bàn phím kích thước to rõ</strong></p><p>Với kích thước bàn phím lớn cho bạn thoải mái nhập liệu văn bản, và cụm số quen thuộc như trên máy tính để bàn sẽ giúp bạn làm việc nhanh hơn.</p><p><strong>Multi Touchpad</strong></p><p><a href=\"https://www.thegioididong.com/hoi-dap/multi-touchpad-la-gi-920569\">Touchpad</a> thông minh chỉ nhận lệnh khi phát hiện đó là đầu ngón tay đang điều khiển và hạn chế được việc lòng bàn tay vô tình chạm vào bị tương tác sai.</p>', '2018-06-10 11:15:18'),
+(4, 1, '<p>Mô tả 2 </p>', '2018-06-10 20:29:16');
 
 -- --------------------------------------------------------
 
@@ -170,6 +216,15 @@ CREATE TABLE `sanpham` (
   `TuDongGiaHan` tinyint(1) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
+--
+-- Đang đổ dữ liệu cho bảng `sanpham`
+--
+
+INSERT INTO `sanpham` (`ID`, `Ten`, `GiaDauGia`, `GiaMuaNgay`, `GioDang`, `ThoiHanBan`, `TrangThai`, `NguoiBan`, `SoLuotRaGia`, `DanhMuc`, `BuocGia`, `TuDongGiaHan`) VALUES
+(1, 'Laptop acer', '13000000', '19000000', '2018-06-10 10:54:29', '2018-06-12 00:00:00', 1, 1, 0, 2, '2000', 0),
+(2, 'Laptop acer', '13000000', '19000000', '2018-06-10 11:14:57', '2018-06-12 00:00:00', 1, 1, 0, 2, '2000', 0),
+(3, 'Laptop acer', '13000000', '19000000', '2018-06-10 11:15:18', '2018-06-12 00:00:00', 1, 2, 0, 2, '2000', 0);
+
 -- --------------------------------------------------------
 
 --
@@ -182,16 +237,20 @@ CREATE TABLE `user` (
   `ADDRESS` varchar(100) NOT NULL,
   `EMAIL` varchar(100) NOT NULL,
   `PHONE` varchar(12) NOT NULL,
-  `DiemDanhGia` int(11) NOT NULL DEFAULT '100',
-  `TYPE` tinyint(1) NOT NULL COMMENT 'Người mua và người bán'
+  `DiemDanhGia` int(11) NOT NULL DEFAULT '0',
+  `TYPE` tinyint(1) NOT NULL COMMENT 'Người mua và người bán',
+  `STATUS` tinyint(1) NOT NULL DEFAULT '1'
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 --
 -- Đang đổ dữ liệu cho bảng `user`
 --
 
-INSERT INTO `user` (`ID`, `NAME`, `ADDRESS`, `EMAIL`, `PHONE`, `DiemDanhGia`, `TYPE`) VALUES
-(1, 'Nguyễn Quang Huy', '145 HB', 'huyqn@gmail.com', '0123456789', 100, 1);
+INSERT INTO `user` (`ID`, `NAME`, `ADDRESS`, `EMAIL`, `PHONE`, `DiemDanhGia`, `TYPE`, `STATUS`) VALUES
+(1, 'Nguyễn Quang Huy', '145 HB', 'huyqn@gmail.com', '0123456789', 0, 1, 1),
+(2, 'Nguyễn Văn Hòa', 'djsahd', 'hjhh', '54545', 0, 0, 1),
+(3, 'Nguyễn Xuân Hiếu', '455', '54', '545', 0, 0, 1),
+(4, 'Nguyễn Văn Hòa', 'ew', 'cotinhtoan@gmail.com', '0906150150', 0, 1, 1);
 
 -- --------------------------------------------------------
 
@@ -212,7 +271,8 @@ CREATE TABLE `xinduocban` (
 --
 
 INSERT INTO `xinduocban` (`ID`, `NguoiDung`, `ThoiGianXin`, `ThoiGianChapNhan`, `TrangThai`) VALUES
-(1, 1, '2018-05-21 12:54:00', '2018-05-22 05:18:08', 1);
+(1, 1, '2018-05-25 14:56:18', '2018-06-07 00:00:00', 1),
+(4, 2, '2018-05-25 15:00:31', '2018-05-25 15:02:06', 1);
 
 --
 -- Chỉ mục cho các bảng đã đổ
@@ -229,7 +289,7 @@ ALTER TABLE `admin`
 --
 ALTER TABLE `caplaimatkhau`
   ADD PRIMARY KEY (`ID`),
-  ADD KEY `FK_CapMatKhau_NguoiDung` (`NguoiDung`);
+  ADD UNIQUE KEY `NguoiDung` (`NguoiDung`);
 
 --
 -- Chỉ mục cho bảng `danhmuc`
@@ -242,8 +302,8 @@ ALTER TABLE `danhmuc`
 --
 ALTER TABLE `danhsachyeuthich`
   ADD PRIMARY KEY (`ID`),
-  ADD KEY `FK_YeuThich_SanPham` (`SanPham`),
-  ADD KEY `FK_YeuThich_NguoiDung` (`NguoiDung`);
+  ADD UNIQUE KEY `NguoiDung` (`NguoiDung`,`SanPham`) USING BTREE,
+  ADD KEY `FK_YeuThich_SanPham` (`SanPham`);
 
 --
 -- Chỉ mục cho bảng `daugia`
@@ -294,6 +354,7 @@ ALTER TABLE `user`
 --
 ALTER TABLE `xinduocban`
   ADD PRIMARY KEY (`ID`),
+  ADD UNIQUE KEY `TrangThai` (`TrangThai`,`NguoiDung`) USING BTREE,
   ADD KEY `FK_XinBan_NguoiDung` (`NguoiDung`);
 
 --
@@ -304,7 +365,7 @@ ALTER TABLE `xinduocban`
 -- AUTO_INCREMENT cho bảng `admin`
 --
 ALTER TABLE `admin`
-  MODIFY `ID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+  MODIFY `ID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=102;
 
 --
 -- AUTO_INCREMENT cho bảng `caplaimatkhau`
@@ -316,31 +377,31 @@ ALTER TABLE `caplaimatkhau`
 -- AUTO_INCREMENT cho bảng `danhmuc`
 --
 ALTER TABLE `danhmuc`
-  MODIFY `ID` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `ID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
 
 --
 -- AUTO_INCREMENT cho bảng `danhsachyeuthich`
 --
 ALTER TABLE `danhsachyeuthich`
-  MODIFY `ID` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `ID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 
 --
 -- AUTO_INCREMENT cho bảng `daugia`
 --
 ALTER TABLE `daugia`
-  MODIFY `ID` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `ID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
 
 --
 -- AUTO_INCREMENT cho bảng `login`
 --
 ALTER TABLE `login`
-  MODIFY `ID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+  MODIFY `ID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
 
 --
 -- AUTO_INCREMENT cho bảng `mota`
 --
 ALTER TABLE `mota`
-  MODIFY `ID` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `ID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
 
 --
 -- AUTO_INCREMENT cho bảng `nhanxet`
@@ -352,19 +413,19 @@ ALTER TABLE `nhanxet`
 -- AUTO_INCREMENT cho bảng `sanpham`
 --
 ALTER TABLE `sanpham`
-  MODIFY `ID` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `ID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 
 --
 -- AUTO_INCREMENT cho bảng `user`
 --
 ALTER TABLE `user`
-  MODIFY `ID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+  MODIFY `ID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
 
 --
 -- AUTO_INCREMENT cho bảng `xinduocban`
 --
 ALTER TABLE `xinduocban`
-  MODIFY `ID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+  MODIFY `ID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
 
 --
 -- Các ràng buộc cho các bảng đã đổ
